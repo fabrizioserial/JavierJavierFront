c595f28a6fd841d31a8eb13ba70601e25264e7dd
fabrizioserial <fabrizio.serial@ing.austral.edu.ar>
2022-07-21 16:45:34 -0300

    feat: update url

--- src/main/Keycloak.js
+++ src/main/Keycloak.js
@@ -1,7 +1,7 @@
 import Keycloak from 'keycloak-js';
 
 const keycloak = new Keycloak({
-    url: "http://localhost:80/keycloak",
+    url: "https://jibberjabber.cloud/keycloak",
     realm: "Jibber-Jabber",
     clientId: "Javier-Javier-Front",
 });
